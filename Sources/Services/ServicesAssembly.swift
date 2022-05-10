402372c00a2501a328c8e8b0663808229c2a9e29
Арман Чархчян <re-mont1@mail.ru>
2022-05-10 22:25:05 +0400

    fix

---
+++ Sources/Services/ServicesAssembly.swift
@@ -0,0 +1,20 @@
+//
+//  File.swift
+//  
+//
+//  Created by Арман Чархчян on 10.05.2022.
+//
+
+import Foundation
+import Swinject
+
+public final class ServicesAssembly: Assembly {
+    
+    public init() { }
+    
+    public func assemble(container: Container) {
+        KeychainServiceAssembly().assemble(container: container)
+        UserDefaultsServiceAssembly().assemble(container: container)
+        CoreDataServiceAssembly().assemble(container: container)
+    }
+}
